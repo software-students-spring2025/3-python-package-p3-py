1c89133d7d3975e6113029f43ee52c58815d43c3
sarahswang <sarah.wang@gmail.com>
2025-03-13 14:34:57 -0400

    motivate() function

--- src/debugBuddy/__main__.py
+++ src/debugBuddy/__main__.py
@@ -1,7 +1,21 @@
 #import debugBuddy.debug as debug
+import random
 
 def main():
     pass
 
 if __name__ == "__main__":
     main()
+
+def motivate(name):
+
+    messages = [
+        f"Keep going, {name}!",
+        f"Hang in there, {name}!",
+        f"Don't give up, {name}!",
+        f"You're almost there, {name}!",
+        f"You're doing amazing, {name}!",
+        
+    ]
+
+    return random.choice(messages)
