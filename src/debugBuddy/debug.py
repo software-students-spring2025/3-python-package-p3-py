04b28a3b760311c379924037915a78fd43b884ee
sarahswang <sarah.wang@gmail.com>
2025-03-13 14:39:54 -0400

    sorry wrong file

--- src/debugBuddy/debug.py
+++ src/debugBuddy/debug.py
@@ -1,1 +1,18 @@
 #File for all of our methods
+import random
+
+def motivate(name):
+
+    messages = [
+
+        f"Hang in there, {name}!",
+        f"Don't give up, {name}!",
+        f"Almost there, {name}!",
+        f"You're one function away from being done, {name}!",
+        f"Think about how satisfied you'll feel when you're done, {name}!",
+        f"One bug at a time, {name}!",
+        f"Your code might be broken, {name}, but you're not! Keep going!",
+
+    ]
+
+    return random.choice(messages)
